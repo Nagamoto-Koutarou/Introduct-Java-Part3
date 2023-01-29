59d3efa427752d0ff62cffb09c9ee60bf9cf42e4
nagamoto <koutaro.n.1056@gmail.com>
2023-01-29 21:09:50 +0900

    chapter17の練習問題

---
+++ src/chapter17/Exercise1And2.java
@@ -0,0 +1,15 @@
+package chapter17;
+
+public class Exercise1And2 {
+    public static void main(String[] args) {
+        String s = null;
+        try {
+            System.out.println(s.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointException例外をcatchしました");
+            System.out.println("ーースタックトレース（ここから）ーー");
+            e.printStackTrace();
+            System.out.println("ーースタックトレース（ここまで）ーー");
+        }
+    }
+}
